b8f8fd02b087136c0b2f92765c5489d2cccc0dcc
Christian Smith <smith.christian.william@gmail.com>
2025-05-16 19:45:57 -0400

    v0.1.0

---
+++ Casks/dumbky.rb
@@ -0,0 +1,17 @@
+cask "dumbky" do
+  version "0.1.0"
+  sha256 :no_check
+
+  url "https://github.com/ChristianWSmith/dumbky/releases/download/v#{version}/dumbky-darwin.zip"
+  name "Dumbky"
+  desc "The Dumb Duck API Tester"
+  homepage "https://github.com/ChristianWSmith/dumbky"
+
+  app "Dumbky.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                     args: ["-d", "com.apple.quarantine", "/Applications/Dumbky.app"],
+                     sudo: true
+  end
+end
